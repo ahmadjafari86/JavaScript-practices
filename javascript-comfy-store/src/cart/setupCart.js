2f932501ee7713395df94121194331f691c162de
Ahmad <60389579+ahmadjafari86@users.noreply.github.com>
2021-10-25 13:44:18 +0330

    commit-19

--- javascript-comfy-store/src/cart/setupCart.js
+++ javascript-comfy-store/src/cart/setupCart.js
@@ -60,8 +60,28 @@ function displayCartItemsDOM() {
   });
 }
 
-function setupCartFunctionality() {
+function removeItem(id) {
+  cart = cart.filter(cartItem => cartItem.id !== id);
+}
 
+function setupCartFunctionality() {
+   cartItemsDOM.addEventListener('click',function (e){
+     const element = e.target;
+     const parent = e.target.parentElement;
+     const id = e.target.dataset.id;
+     const parentId = e.target.parentElement.dataset.id;
+     // remove
+     if (element.classList.contains('cart-item-remove-btn')){
+       removeItem(id);
+       parent.parentElement.remove();
+     }
+     // increase
+     // decrease
+
+     displayCartItemCount();
+     displayCartTotal();
+     setStorageItem('cart',cart);
+   });
 }
 const init = ()=> {
   displayCartItemCount();
